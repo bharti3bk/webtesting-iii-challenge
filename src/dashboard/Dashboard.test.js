9a61e59e52419892355dce3b714f45cbc9712d77
bharti3bk <bharti3bk@gmail.com>
2019-08-19 11:18:58 -0700

    started project

---
+++ src/dashboard/Dashboard.test.js
@@ -0,0 +1,13 @@
+// Test away 
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Dashboard from './Dashboard'  
+import {render , fireEvent} from '@testing-library/react';
+
+describe('<Dashboard/>', () => {
+    it('should match snapshot', () => {
+      const tree = renderer.create(<Dashboard />).toJSON();
+  
+      expect(tree).toMatchSnapshot();
+    });
+  });  
